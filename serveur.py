8ba46d0679b52c6a04938c148f9c43fa29923d6d
Eya Khsiba <eyakhsiba20@gmail.com>
2025-12-16 07:26:01 +0100

    Delete serveur.py

--- serveur.py
+++
@@ -1,7 +0,0 @@
-from http.server import SimpleHTTPRequestHandler
-from socketserver import TCPServer
-
-PORT = 8000
-with TCPServer(("", PORT), SimpleHTTPRequestHandler) as httpd:
-    print("Serveur OTA actif sur le port", PORT)
-    httpd.serve_forever()
